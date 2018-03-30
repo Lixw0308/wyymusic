4dc1817ff3ac1379e66c1c31c676abf3a72d13e4
DC-king <775828639@qq.com>
2018-03-30 22:19:23 +0800

    杜金山-index_player.html

--- assets/js/index_player.js
+++ assets/js/index_player.js
@@ -1,5 +1,6 @@
 var palyer_status = "stop";
 var player_position = "1";
+var left_span = $("#scroll_box").offset().left;
 $(".button_play_a_song").on("click", function() {
     if (palyer_status == "play") {
         palyer_status = "stop";
@@ -16,9 +17,17 @@ $(".button_play_a_song").on("click", function() {
 })
 $("#scroll_box").on('click', function(ev) {
     var ev = event;
-    // console.log(parseInt((ev.clientX - 48) / 2.7) + "%");
+    var percent_time = parseInt((ev.clientX - left_span) / 2.7);
+    console.log(percent_time + "%");
+    chenge_scroll_ball_position(percent_time);
+    return percent_time;
 })
 $(".scroll_ball").on('mousedown', function(ev) {
     var ev = event;
-    console.log(ev.clientX - 48);
+    console.log(ev.clientX - left_span + 2.25);
 })
+
+function chenge_scroll_ball_position(percent_time) {
+    var ball_position = 2.72 * percent_time - 2.25;
+    $(".scroll_ball").css({ left: ball_position })
+}
