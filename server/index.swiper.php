0660dd43bc850d68cda5062d7f935fb2ea8ba646
y122972 <y122972@gmail.com>
2018-04-01 20:27:22 +0800

    首页swiper通过ajax获取，添加index.swiper.php by yjm

---
+++ server/index.swiper.php
@@ -0,0 +1,21 @@
+<?php
+
+    echo'{"errCode": 1,
+        "errMsg": "", 
+        "info": [{"id": 1,
+            "img_url": "http://p1.music.126.net/hCzuP6QOvWc6T3irijp3zw==/109951163213915707.jpg"},
+            {"id": 2,
+                "img_url": "http://p1.music.126.net/2d_gTxI1zsWa0wmNz3DSNw==/109951163213468979.jpg"},
+            {"id": 3,
+                "img_url": "http://p1.music.126.net/FY2YmzSEnTYgP-Q-6yKzKQ==/109951163214196078.jpg"},
+            {"id": 4,
+                "img_url": "http://p1.music.126.net/oH2YsSkCuKzd1tOkwU0QlQ==/109951163213906153.jpg"},
+            {"id": 5,
+                "img_url": "http://p1.music.126.net/hCzuP6QOvWc6T3irijp3zw==/109951163213915707.jpg"},
+            {"id": 6,
+                "img_url": "http://p1.music.126.net/2d_gTxI1zsWa0wmNz3DSNw==/109951163213468979.jpg"},
+            {"id": 7,
+                "img_url": "http://p1.music.126.net/FY2YmzSEnTYgP-Q-6yKzKQ==/109951163214196078.jpg"},
+            {"id": 8,
+                "img_url": "http://p1.music.126.net/oH2YsSkCuKzd1tOkwU0QlQ==/109951163213906153.jpg"}
+        ]}';
